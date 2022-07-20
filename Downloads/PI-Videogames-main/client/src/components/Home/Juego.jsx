3a47d0f8a2867fc404b6173f20b53caa2a423108
Ulysses <94791119+Blob2142@users.noreply.github.com>
2022-07-20 13:17:42 -0300

    Update Juego.jsx

--- Downloads/PI-Videogames-main/client/src/components/Home/Juego.jsx
+++ Downloads/PI-Videogames-main/client/src/components/Home/Juego.jsx
@@ -2,12 +2,12 @@ import "./home_css.css"
 import logo from "../../img/logo.jpg";
 import { NavLink } from "react-router-dom"
 
-function Juego ({name, img, generos, id, buscar, clear}) {
+function Juego ({name, img, generos, id}) {
     return(
         <>
         <div className="Contein">
         <img src={`${img || logo}`} className="img" width="95%" height="150" alt="img"/>
-        <NavLink to={`/detail`} onClick={() => {clear() && buscar(id)}} className="gameName" >
+        <NavLink to={`/detail/${id}`} className="gameName" >
             {name}
         </NavLink>
         <p className="generos">
